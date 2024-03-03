efa854dc914c8db5ad0ae52d357d47bb4b33a418
Patricio Farias <pignacio.farias@srmappy.cl>
2024-03-03 02:58:32 -0300

    primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let PrecioInicial =400000;
+let CantidadInicial =1;
+
+let Preciobase = document.querySelector(".Preciobase");
+let Cantidad = document.querySelector(".Cantidad");
+let Total =document.querySelector(".Total");
+
+Preciobase.innerHTML = PrecioInicial;
+Cantidad.innerHTML = CantidadInicial;
+Total.innerHTML = PrecioInicial;
